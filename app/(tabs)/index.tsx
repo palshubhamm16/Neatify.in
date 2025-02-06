13b631e1d5eec8c0606551c4db1b70457bcbe12a
Akshat <akshttt.dev@gmail.com>
2025-02-06 13:53:34 +0530

    UI fix and Profile Screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View } from "react-native";
 const HomeScreen = () => {
   return (
     <View style={styles.container}>
-      <Text>HomeScreen</Text>
+      <Text >HomeScreen</Text>
     </View>
   );
 };
